1662f9b0d3aba46c4c4b178b12993d621a79fa33
changyeol <cksruf91@gmail.com>
2022-04-23 16:30:21 +0900

    bypass mlflow issues

--- model/callbacks.py
+++ model/callbacks.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 import mlflow
 import numpy as np
@@ -54,7 +55,7 @@ class TrainHistory:
 class MlflowLogger:
 
     def __init__(self, experiment_name: str, model_params: dict, run_name=None, run_id=None, log_model=False,
-                 model_name='torch_model'):
+                 model_name='torch_model', monitor='val_loss', mode='min'):
         self.experiment_name = experiment_name
         self.run_name = run_name
         self.model_params = model_params
@@ -63,13 +64,22 @@ class MlflowLogger:
         self._set_env()
         self.run_id = self._get_run_id() if run_id is None else run_id
 
+        init_values = {'min': np.inf, 'max': -np.inf}
+        self.mode = mode
+        self.monitor = monitor
+        self.best_score = init_values[self.mode]
+
     def __call__(self, model, history):
         with mlflow.start_run(run_id=self.run_id):
             if self.log_model:
-                mlflow.pytorch.log_model(
-                    pytorch_model=model,
-                    artifact_path=self.model_name
-                )
+                if self._best(history[self.monitor], self.best_score):
+                    self._truncate_log_model_history()
+                    mlflow.pytorch.log_model(
+                        pytorch_model=model,
+                        artifact_path=self.model_name
+                    )
+                    self.best_score = history[self.monitor]
+
             mlflow.log_metrics(history, step=history['epoch'])
 
     def __eq__(self, other):
@@ -86,3 +96,25 @@ class MlflowLogger:
             raise ValueError("Environment variable MLFLOW_TRACKING_URI is not exist")
 
         mlflow.set_experiment(self.experiment_name)
+
+    def _best(self, val, best):
+        if self.mode == 'min':
+            return val <= best
+        else:
+            return val >= best
+
+    def _truncate_log_model_history(self):
+        """
+        On each epoch mlflow.pytorch.log_model append log to 'mlflow.log-model.history' tag
+        after several epoch this tag becomes very large and exceeds the length limit.
+        so manually truncate log from tags table issues
+        reference github issues : https://github.com/mlflow/mlflow/issues/3032
+        """
+        sql = f'''
+        UPDATE tags SET value = '[]' 
+         WHERE run_uuid = '{self.run_id}'
+           AND key = 'mlflow.log-model.history'
+        '''
+        with sqlite3.connect('~/mydb.sqlite') as conn:  # sqlite:///mydb.sqlite
+            cur = conn.cursor()
+            cur.execute(sql)
